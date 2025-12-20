cc0bf355e9ec8cc04513b1dd93629bf7aa497a34
liyl <2416053677@qq.com>
2025-12-20 17:28:54 +0800

    feat(advice): 添加TokenResponseAdvice用于响应头中添加Token
    实现统一响应增强，在响应头中自动添加Token信息，便于前端获取和更新认证信息

---
+++ niro-server/niro-core/src/main/java/com/niro/core/advice/TokenResponseAdvice.java
@@ -0,0 +1,54 @@
+package com.niro.core.advice;
+
+import cn.dev33.satoken.stp.StpUtil;
+import com.niro.core.result.Result;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * 统一响应增强，用于在响应头中添加Token
+ *
+ * @author liyl
+ * @date 2025/12/20
+ */
+@ControllerAdvice
+public class TokenResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        // 只有返回Result类型的接口才进行处理，或者可以全部处理
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        
+        // 获取 HttpServletResponse
+        if (response instanceof ServletServerHttpResponse) {
+            HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+            
+            // 获取当前 Token
+            try {
+                String tokenValue = StpUtil.getTokenValue();
+                if (tokenValue != null) {
+                    servletResponse.setHeader("Authorization", "Bearer " + tokenValue);
+                    servletResponse.setHeader("niro-token-update", tokenValue);
+                    servletResponse.addHeader("Access-Control-Expose-Headers", "Authorization, niro-token-update");
+                }
+            } catch (Exception ignored) {
+                // 忽略异常，可能是未登录状态调用
+            }
+        }
+        
+        return body;
+    }
+}
